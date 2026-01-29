b225f6166c91748c14c283c6d0a56f6e0b711f9b
chryzller john suing <mcjsuing@tip.edu.ph>
2026-01-29 11:16:16 +0800

    Add API and Conditional

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import UserList from './UserList'; // Import added
+import UserList from './UserList';
+import Counter from './Counter';
+import UserAPI from './UserAPI';
 
 function App() {
   const users = ['Alice', 'Bob', 'Charlie'];
+  const isLoggedIn = true;
 
   return (
     <div>
       <h1>React Lab</h1>
+      {/* Conditional Rendering */}
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
+
+      <Counter />
       <UserList users={users} />
+
+      <h2>API Data:</h2>
+      <UserAPI />
     </div>
   );
 }
